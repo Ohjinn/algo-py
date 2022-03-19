fd5da72fc16e28cbd2fe468c647f7447d9a827c1
Ohjinn <hjin9445@gmail.com>
2022-03-19 23:57:43 +0900

    220319

---
+++ programmers/stack_queue/프린터.py
@@ -0,0 +1,43 @@
+def solution(priorities, location):
+    answer = 0
+    length = len(priorities)
+    check = [0 for i in range(length)]
+    j = 0
+
+    while check[location] == 0:
+        max = 0
+        num_of_max = 0
+        print(check)
+        print(priorities)
+        for i in range(length):
+            if priorities[i] > max and check[i] == 0:
+                max = priorities[i]
+
+        for i in range(length):
+            if priorities[i] == max:
+                num_of_max += 1
+        print(max)
+        print("nom", num_of_max)
+
+        for j in range(j, length):
+            print("j = ", j)
+            if priorities[j] >= max and check[j] == 0:
+                check[j] = 1
+                answer += 1
+                num_of_max -= 1
+                print(answer)
+                print("nom", num_of_max)
+                if num_of_max == 0:
+                    break
+
+            if check[location] == 1:
+                break
+        if j == length - 1:
+            j = 0
+
+    print(check)
+    return answer
+
+
+print(solution([1, 1, 9, 1, 1, 1], 0))
+
