bc24f2679fbeac8e2ef5eb92eff37de37b36b423
Ohjinn <hjin9445@gmail.com>
2022-04-19 00:56:27 +0900

    220419

--- programmers/dp/등굣길.py
+++ programmers/dp/등굣길.py
@@ -1,14 +1,24 @@
 def solution(m, n, puddles):
-    answer = 0
-    dp = [[1] * (m + 1) for _ in range(n + 1)]
-    dp[1][1] = 0
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    dp[1][1] = 1
     for puddle in puddles:
-        dp[puddle[1]][puddle[0]] = 0
+        dp[puddle[1]][puddle[0]] = -1
 
     for i in range(1, n + 1):
         for j in range(1, m + 1):
+            if i == 1 and j == 1:
+                dp[1][1] = 1
+            elif dp[i][j] == -1:
+                continue
+            elif dp[i - 1][j] == -1 or dp[i][j - 1] == -1:
+                if dp[i - 1][j] == -1:
+                    dp[i][j] = dp[i][j - 1]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+            else:
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007
 
-    return answer
+    return dp[n][m]
 
-solution(4, 3, [[2, 2]])
+print(solution(4, 3, [[2, 2]]))
 
