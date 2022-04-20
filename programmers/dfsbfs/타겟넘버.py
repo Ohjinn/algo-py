9426da85532e68c0ae685945a50609f777ef6e33
Ohjinn <hjin9445@gmail.com>
2022-04-20 23:35:05 +0900

    220420

---
+++ programmers/dfsbfs/타겟넘버.py
@@ -0,0 +1,23 @@
+answer = 0
+
+
+def dfs(numbers, depth, sum_of, target):
+    global answer
+    if depth == len(numbers):
+        if sum_of == target:
+            answer += 1
+    else:
+        dfs(numbers, depth + 1, sum_of + numbers[depth], target)
+        dfs(numbers, depth + 1, sum_of - numbers[depth], target)
+
+
+def solution(numbers, target):
+    global answer
+    depth, sum_of = 0, 0
+
+    dfs(numbers, depth, sum_of, target)
+    return answer
+
+
+print(solution([4, 1, 2, 1], 4))
+
