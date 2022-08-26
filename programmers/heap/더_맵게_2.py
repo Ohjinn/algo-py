cb0542fcc144bf46e0a1ce72b6ca9c8769e733d6
Ohjinn <hjin9445@gmail.com>
2022-08-26 23:55:18 +0900

    220826

---
+++ programmers/heap/더_맵게_2.py
@@ -0,0 +1,13 @@
+import heapq
+
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+
+    while scoville[0] < K and len(scoville) > 1:
+        mixed = heapq.heappop(scoville) + heapq.heappop(scoville) * 2
+        heapq.heappush(scoville, mixed)
+        answer += 1
+
+    return answer if scoville[0] >= K else -1
