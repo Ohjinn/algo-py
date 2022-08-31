d78b145df0c98cbe70de29699e9f0fd1e8a42776
Ohjinn <hjin9445@gmail.com>
2022-08-31 23:41:17 +0900

    220831

---
+++ programmers/sort/KthNumber2.py
@@ -0,0 +1,18 @@
+
+def solution(array, commands):
+    answer = []
+
+    for command in commands:
+        i = command[0]
+        j = command[1]
+        k = command[2]
+
+        cutList = array[i - 1, j]
+        cutList.sort()
+        answer.append(cutList[k-1])
+    return answer
+
+
+solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]])
+
+# def quick_sort(array):
