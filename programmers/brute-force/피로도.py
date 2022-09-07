5689ef02e8254f6169fcf50b99af63e511afc3b6
Ohjinn <hjin9445@gmail.com>
2022-09-07 23:39:30 +0900

    220907

--- programmers/brute-force/피로도.py
+++ programmers/brute-force/피로도.py
@@ -1,4 +1,8 @@
+from itertools import permutations
+
+
 def dfs(k, dungeons, visited, cnt, answer):
+    print(visited)
     for i in range(len(dungeons)):
         if visited[i] != 1 and dungeons[i][0] <= k:
             visited[i] = 1
@@ -9,6 +13,7 @@ def dfs(k, dungeons, visited, cnt, answer):
     return answer
 
 
+# dfs 이용
 def solution(k, dungeons):
     visited = [0 for i in range(len(dungeons))]
 
@@ -16,4 +21,21 @@ def solution(k, dungeons):
     return answer
 
 
-print(solution(80, [[80, 20], [50, 40], [30, 10]]))
+# permutation 이용
+def solution_with_permutation(k, dungeons):
+    answer = -1
+    n = len(dungeons)
+
+    for permutation in list(permutations(range(0, n), n)):
+        cnt = 0
+        temp_k = k
+        for i in permutation:
+            if temp_k >= dungeons[i][0]:
+                temp_k -= dungeons[i][1]
+                cnt += 1
+        answer = max(answer, cnt)
+    return answer
+
+
+# print(solution(80, [[80, 20], [50, 40], [30, 10]]))
+print(solution_with_permutation(80, [[80, 20], [50, 40], [30, 10]]))
