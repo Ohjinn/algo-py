a29bf8c0ca0bdd53a7a12b9a038fd8aab6fd4747
Ohjinn <hjin9445@gmail.com>
2022-12-02 00:20:09 +0900

    221201

--- programmers/basic100/분수의_덧셈.py
+++ programmers/basic100/분수의_덧셈.py
@@ -1,15 +1,36 @@
 import math
 
 
+def gcd(a, b):
+    while a % b != 0:
+        mod = a % b
+        a = b
+        b = mod
+    return b
+
+# 더 나눠질 수 있는 분수가 답으로 나오는 문제
 def solution(denum1, num1, denum2, num2):
-    num3, denum3 = denum1 * num2 + num1 * denum2, denum1 * denum2
-    print(num3, denum3)
-    gcd = math.gcd(max(denum1, denum2), min(denum1, denum2))
-    answer = [num3 // gcd, denum3 // gcd]
+    denum3, num3 = denum1 * num2 + num1 * denum2, num1 * num2
+    gcd = math.gcd(num2, num1)
+    answer = [denum3 // gcd, num3 // gcd]
+
+    a, b = answer[0], answer[1]
+
     return answer
 
+# 1, 2, 1, 2 -> 2/2로 해결 불가능
+def solution2(denum1, num1, denum2, num2):
+    denum3, num3 = denum1 * num2 + num1 * denum2, num1 * num2
+    m = 1
+    for i in range(1, denum3):
+        if denum3 % i == 0 and num3 % i == 0:
+            m = i
+    ml = gcd(denum3 / m, num3 / m)
 
-print(solution(1, 2, 3, 4))
+
+    return [(int)(denum3 / ml), (int)(num3 / ml)]
+
+print(solution2(1, 2, 1, 2))
 
 
 '''
