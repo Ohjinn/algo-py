dc2d5d48dfbd828acba41b457c8dd9553bb21d82
Ohjinn <hjin9445@gmail.com>
2023-01-21 23:58:28 +0900

    230121

--- programmers/level1/숫자_짝꿍.py
+++ programmers/level1/숫자_짝꿍.py
@@ -1,6 +1,18 @@
 def solution(X, Y):
-    answer = ''
-    return answer
+    result_arr = []
+    for i in X:
+        if i in Y:
+            Y = Y.replace(i, '', 1)
+            result_arr.append(i)
+    result_arr.sort(reverse = True)
+    answer = ''.join(result_arr)
+
+    if len(answer) != 0 and int(answer) != 0:
+        return answer
+    elif int(answer) == 0:
+        return '0'
+    else:
+        return '-1'
 
 
 print(solution("100", "2345"))
