f70d90564776f515ea810eacbd1d3a8ad874dcc7
Ohjinn <hjin9445@gmail.com>
2023-01-25 23:33:29 +0900

    230125

--- programmers/level1/실패율.py
+++ programmers/level1/실패율.py
@@ -21,4 +21,17 @@ def solution(N, stages):
     return [i[0] for i in answer]
 
 
+def solution2(N, stages):
+    result = {}
+    denominator = len(stages)
+    for stage in range(1, N+1):
+        if denominator != 0:
+            count = stages.count(stage)
+            result[stage] = count / denominator
+            denominator -= count
+        else:
+            result[stage] = 0
+    return sorted(result, key=lambda x : result[x], reverse=True)
+
+
 print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
