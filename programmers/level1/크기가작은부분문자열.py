7f74d8b8f64696576402d8de728211332929bd76
Ohjinn <hjin9445@gmail.com>
2023-02-01 23:55:37 +0900

    230201

---
+++ programmers/level1/크기가작은부분문자열.py
@@ -0,0 +1,12 @@
+from itertools import permutations
+
+
+def solution(t, p):
+    answer = 0
+    permute = permutations(t, len(p))
+    for tuple in list(permute):
+
+    return answer
+
+
+print(solution("3141592", "271"))
