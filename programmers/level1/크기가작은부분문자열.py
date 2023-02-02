2c6efe1c97cc1fbb62d33e0696ae300532d368e4
Ohjinn <hjin9445@gmail.com>
2023-02-02 23:20:26 +0900

    230202

--- programmers/level1/크기가작은부분문자열.py
+++ programmers/level1/크기가작은부분문자열.py
@@ -1,12 +1,24 @@
-from itertools import permutations
+from itertools import combinations
 
 
+# 문제 잘못 이해
 def solution(t, p):
     answer = 0
-    permute = permutations(t, len(p))
+    permute = combinations(t, len(p))
     for tuple in list(permute):
-
+        word_sum = ''.join(tuple)
+        print(word_sum)
     return answer
 
 
-print(solution("3141592", "271"))
+def solution2(t, p):
+    answer = 0
+    p_length = len(p)
+    for i in range(0, len(t) - p_length + 1):
+        if p >= t[i:i + p_length]:
+            answer += 1
+    return answer
+
+
+print(solution2("3141592", "271"))
+print(solution2("500220839878", "7"))
