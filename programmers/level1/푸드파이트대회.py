c2bf6c40420d6344242460fd1b174e2db5b42f17
Ohjinn <hjin9445@gmail.com>
2023-02-04 23:17:05 +0900

    230204

---
+++ programmers/level1/푸드파이트대회.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+
+def solution(food):
+    answer = ''
+    a = deque([0])
+    for i in range(len(food) - 1, -1, -1):
+
+    return answer
+
+
+print(solution([1, 3, 4, 6]))
