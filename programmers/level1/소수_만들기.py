f584cdedda74990e7759547d9c276d2b58947a57
Ohjinn <hjin9445@gmail.com>
2023-02-07 23:51:28 +0900

    230207

--- programmers/level1/소수_만들기.py
+++ programmers/level1/소수_만들기.py
@@ -2,12 +2,12 @@ from itertools import combinations
 
 
 def solution(nums):
-    combination = combinations(nums, 3)
-    answer = len(list(combination))
+    combination = list(combinations(nums, 3))
+    answer = len(combination)
 
     for i in combination:
-        for j in range(2, sum(i) + 1):
-            if sum(i) % j != 0:
+        for j in range(2, sum(i)):
+            if sum(i) % j == 0:
                 answer -= 1
                 break
     return answer
