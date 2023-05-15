0297f337134f700d3f2769d63444f12a6fb6047c
Ohjinn <hjin9445@gmail.com>
2023-05-15 23:56:15 +0900

    230515

--- programmers/level2/게임맵최단거리.py
+++ programmers/level2/게임맵최단거리.py
@@ -1,4 +1,6 @@
 from collections import deque
+
+
 # checking_table = [[0] * size_of_map for _ in range(size_of_map)]
 
 
@@ -23,10 +25,9 @@ def bfs(maps, x, y):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]) and maps[nx][ny] != 0:
-                if maps[nx][ny] == 1:
-                    maps[nx][ny] = maps[x][y] + 1
-                    next_queue.append([nx, ny])
+            if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]) and maps[nx][ny] != 0 and maps[nx][ny] == 1:
+                maps[nx][ny] = maps[x][y] + 1
+                next_queue.append([nx, ny])
 
     return maps[-1][-1] if maps[-1][-1] > 1 else -1
 
@@ -35,4 +36,4 @@ def solution(maps):
     return bfs(maps, 0, 0)
 
 
-print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
+print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]))
